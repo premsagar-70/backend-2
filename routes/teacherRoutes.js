cb6327500d3a063224efa4ad9f78c808f65372e0
premsagar-70 <premsagarmahasamudhram@gmail.com>
2025-04-22 00:26:07 +0530

    Backend Full Code

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { addteacher, getteacher } = require("../controllers/teacherController");
 
 const router = express.Router();
-router.post("/add", addteacher);
+router.post("/users/add", addteacher);
 router.get("/", getteacher);
 
 module.exports = router;
