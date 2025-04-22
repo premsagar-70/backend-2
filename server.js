ac4e4c69e16d3f5a304f13f514d5532d3086fb31
premsagar-70 <premsagarmahasamudhram@gmail.com>
2025-04-22 18:25:05 +0530

    Backend Full Code

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(cors({
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization"
 }));
+app.options('*', cors()); // <<<--- ADD THIS LINE
 
 // API Routes
 app.use("/api/auth", authRoutes);
